1f300d468cceb71e02f1232a260143c1d8ef1fee
Naman Agrawal <namanagrawal1895@gmail.com>
2022-08-23 22:11:26 +0530

    Added readme files

--- BlackJack/BlackJack.cpp
+++ BlackJack/BlackJack.cpp
@@ -1,33 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-    Theory
-    BlackJack Game
-    It is a 2 player game, where one of the player is CPU dealer and other is the user.
-    In this game, some amount of money is their with the user. There is a deck of cards. User always starts
-    first in the game. User and CPU have 2 cards each initially. 
-    Every player (user or CPU) has to make 2 choices
-        1. Hit (picks up a card from the deck)
-        2. Stay (do not pick up)
-    Both the players try to bring their cards sum closer to 21 (but less than 21 only)
-
-    Now as user starts the turn, user decides whether to pick up a card or not .
-    If the user cards sum went over 21 while picking, then user loses.
-    If user stays, then now is the turn of CPU
-    CPU picks the cards until it either beat the player or busts.
-
-    In the end, the player with the sum closest to 21 (<=21) wins.
-    If any player has sum over 21, that player is busted and the other player wins
-    If user loses, then the user loses all the money the user bets
-    If dealer loses, then user recieves double the bet money and wins
-    
-    Note that ace can be either considered as 11 or 1 according to the requirement 
-    Jack Queen King are considered as 10
-
-    In our simpler version of BlackJack, on the dealer's turn, dealer always pick until dealer busts,
-    or dealer beats the player
-*/
 bool isNumber(string &s){
     for(int i=0;i<s.length();i++){
         if(!isdigit(s[i]))
--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,33 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/* 
-    Welcome to the game of Tic-Tac-Toe
-    The game is designed to played by 2 players
-    Its a normal TicTacToe game where one player chooses X and the other gets O or vice-versa
-    Whenever a player wins, the game is ended and the name of the player is printed
-*/
-
-/*
-    things to do for easier implementation
-    first print welcome to tictactoe
-    then ask for the names of the players
-    then randomly ask anyone to choose first and assign another automatically
-    then the game starts
-
-    ask the first player to make a move by choosing number from 1-9
-    if a player chooses occupied position ask the person again to enter valid position
-    then ask 2nd player to move
-    then 2nd player chooses a position and place the symbol of this player accordingly
-    always check for valid positions
-    every time a move is made print the resulting board to help next player to move
-
-    once you get 3 similar values in any row,column or diagonal print the corresponding player to have won the game
-    and game ends
-
-    ask both the users whether to play or not and both the players should accept for next turn to continue the game
-*/
-
 // implementation
 void print(vector<vector<char>> &matrix)
 {
